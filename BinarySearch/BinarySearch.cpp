b2d8e5b43cdaaa28bc666ea40863f9e11084022b
Harshita Thakur <harshitathakur2806@gmail.com>
2026-07-08 00:16:25 +0530

    Binary Search Questions

--- BinarySearch/BinarySearch.cpp
+++ BinarySearch/BinarySearch.cpp
@@ -1,21 +1,39 @@
 #include<iostream>
 using namespace std;
-int binarySearch(int arr[], int size, int key) {
-    int start = 0;
-    int end = size - 1;
-
-    while (start <= end) {
-        int mid = start + (end - start) / 2;
-
-        if (arr[mid] == key) {
-            return mid; // Key found at index mid
+int main() {
+    int n;
+    cout<<"Enter the number of elements in the array: ";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter the elements of the array: ";
+    for(int i=0; i<n; i++) {
+        cin>>arr[i];
+    }
+    int target;
+    cout<<"Enter the target element to search: ";
+    cin>>target;
+    int low = 0;
+    int high = n - 1;
+    int mid;
+    bool found = false;
+    while (low <= high) {
+        mid =(low+high) / 2;
+        if (arr[mid] == target) {
+            found = true;
+            break; 
         }
-        else if (arr[mid] < key) {
-            start = mid + 1; // Search in the right half
+        else if (arr[mid] < target) {
+            low = mid + 1; 
         }
         else {
-            end = mid - 1; // Search in the left half
+           high = mid - 1; 
         }
     }
-    return -1; // Key not found
+    if (!found) {
+        cout<<"Element not found in the array."<<endl;
+    }
+    else {
+        cout<<"Element found at index: "<<mid<<endl;
+    }
+    return 0;
 }
